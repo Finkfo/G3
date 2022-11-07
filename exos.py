1a2382813715226b96cddc6aa2df1d74359c8c7d
Finkfo <valentin.valleepro@gmail.com>
2022-11-07 18:58:23 +0100

    kick off

---
+++ exos.py
@@ -0,0 +1,31 @@
+#add(x,y)
+#sub(x,y)
+#multiply(x,y)
+#divide(x,y)
+#modulo(x,y)
+
+def add(x,y):
+    return x+y
+
+def sub(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
+def modulo(x,y):
+    return x%y
+
+def salaireNet(salaireBrut, coefficient):
+    return salaireBrut*coefficient
+
+def salaireParSeconde(SalaireHoraire, HeureParJourOuvre, NbJoursOuvreParAn):
+
+
+#SalaireParSeconde(
+# SalaireHoraire,
+# heureParJourOuvre,
+# NbJoursOuvréParAn)
