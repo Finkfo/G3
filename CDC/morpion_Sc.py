bcd5ada5af952ec143422078d67760c068aac16f
FinkSo <vvallee@gaming.tech>
2022-11-16 10:02:53 +0100

    SC morpions

---
+++ CDC/morpion_Sc.py
@@ -0,0 +1,21 @@
+#Definir la fonction afficher_grille(grille) qui creer la grile de jeu du morpion
+    #Ecrire
+
+#Definir la fontion est_gagnant(grille) qui retourne si il y a un gagnant
+    #Si grille[0] est égal à grille[1] et grille[0] est égal à grille[2] et que grille[0] est different de " "
+        #Alors retourner 1
+    #Si grille[3] est égal à grille[4] et grille[3] est égal à grille[5] et que grille[3] est different de " "
+        #Alors retourner 1
+    #Si grille[6] est égal à grille[7] et grille[6] est égal à grille[8] et que grille[6] est different de " "
+        #Alors retourner 1
+    #Si grille[0] est égal à grille[3] et grille[6] est égal à grille[5] et que grille[0] est different de " "
+        #Alors retourner 1
+    #Si grille[1] est égal à grille[4] et grille[1] est égal à grille[7] et que grille[1] est different de " "
+        #Alors retourner 1
+    #Si grille[2] est égal à grille[5] et grille[2] est égal à grille[8] et que grille[2] est different de " "
+        #Alors retourner 1
+    #Si grille[0] est égal à grille[4] et grille[0] est égal à grille[8] et que grille[0] est different de " "
+        #Alors retourner 1
+    #Si grille[2] est égal à grille[4] et grille[2] est égal à grille[6] et que grille[2] est different de " "
+        #Alors retourner 1
+    
